bec590136ccbc5a0cc44a39ac35f5b35118a3af2
Bilal Amarni <bilal.amarni@gmail.com>
2016-07-03 19:58:25 +0200

    Initial commit

---
+++ snitch.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	var args []string
+	argsLen := len(os.Args)
+	if argsLen < 2 {
+		log.Fatal("Not enough arguments.")
+	} else if argsLen > 2 {
+		args = os.Args[2:]
+	}
+
+	cmd := exec.Command(os.Args[1], args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	childPid := make(chan int, 1)
+	childExited := make(chan error, 1)
+
+	go func() {
+		if err := cmd.Start(); err != nil {
+			log.Fatalf("Couldn't start command : %s", err)
+		}
+		childPid <- cmd.Process.Pid
+		childExited <- cmd.Wait()
+	}()
+
+	pid := <-childPid
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig)
+	for {
+		select {
+		case signal := <-sig:
+			// ignore SIGCHLD
+			if signal == syscall.SIGCHLD {
+				continue
+			}
+
+			syscall.Kill(pid, signal.(syscall.Signal))
+
+		case err := <-childExited:
+			if err == nil {
+				os.Exit(0)
+			}
+
+			if exitErr, ok := err.(*exec.ExitError); ok {
+				if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+					os.Exit(status.ExitStatus())
+				}
+			}
+
+			log.Fatalf("Command didn't run as expected : %s", err)
+		}
+	}
+}
